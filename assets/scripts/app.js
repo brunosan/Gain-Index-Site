dcd6bf4e6ce71401149fc1289ee72a72f3213e24
Alex Barth <alex@developmentseed.org>
2011-08-05 16:47:09 -0400

    Modify backbone's root to ensure compatibility with server side routing. Backbone's default root ('/') would force us to use routes not starting with '/', this in turn would make routes useless for connect.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 $(function() {
-    Bones.start({pushState: true});
-    // TODO: find out why route is executed automatically on '/'.
+    // Start backbone with root = '', we'll need to start all routes with /
+    // anyway to stay compatible with connect.
+    Bones.start({pushState: true, root: ''});
     var path = location.pathname + location.search;
     path != '/' && Backbone.history.navigate(path, true);
 });
