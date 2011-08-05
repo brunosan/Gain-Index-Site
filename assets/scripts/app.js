21cda0748293bfaedf8d39aa9e83d53f3223a45d
Alex Barth <alex@developmentseed.org>
2011-08-05 16:57:47 -0400

    Add key tracking, corresponding code that uses the window.currentKeys flag is already committed.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,16 @@
 $(function() {
+    // Global tracking of pressed keys.
+    $(document).keydown(function(ev) {
+        window.currentKeys = window.currentKeys || {};
+        window.currentKeys[ev.keyCode] = ev;
+    });
+    $(document).keyup(function(ev) {
+        window.currentKeys = window.currentKeys || {};
+        if (window.currentKeys[ev.keyCode]) {
+            delete window.currentKeys[ev.keyCode];
+        }
+    });
+
     // Start backbone with root = '', we'll need to start all routes with /
     // anyway to stay compatible with connect.
     Bones.start({pushState: true, root: ''});
